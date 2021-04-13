fe25e7796fd0a049b71212ee429bdd435906808a
majuszko <majamrozinska97@gmail.com>
2021-04-13 08:08:36 +0200

    Removed flag colliders

--- Assets/Resources/Scripts/Flag.cs
+++ Assets/Resources/Scripts/Flag.cs
@@ -6,27 +6,19 @@ using UnityEngine;
 public class Flag : MonoBehaviour
 {
     [SerializeField] GameManager gm;
-    [SerializeField] GameObject ground;
-    private BoxCollider2D[] bc2d;
+    [SerializeField] private GameObject ground;
     
-
     private void Start()
     {
-        bc2d = GetComponentsInChildren<BoxCollider2D>();
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
         ground = GameObject.Find("Ground");
-
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         gm.BallInHole();
         ground.GetComponent<BoxCollider2D>().enabled = false;
-        foreach (BoxCollider2D bc in bc2d)
-        {
-            bc.enabled = true;
-        }
-
-}
+        
+    }
 
 }
